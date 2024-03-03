fccd6ee17acda7aec3295d569e8c844d1d47221c
Ali Hodroj <aloshito2004@gmail.com>
2024-03-03 20:04:32 +0200

    Initial Commit

---
+++ Trivy/TrivyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrivyApp.swift
+//  Trivy
+//
+//  Created by Ali Hodroj on 03/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct TrivyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
